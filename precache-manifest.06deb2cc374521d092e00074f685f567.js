0395220bc94c3353da8e7ce6869eb320aeaf7af3
Diego Mattos <dmattos1855@gmail.com>
2019-06-17 17:22:29 -0300

    Fix css

---
+++ precache-manifest.06deb2cc374521d092e00074f685f567.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e94cbf9cc478502b1503e025b15ac7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83f5455322dfa709d2d2",
+    "url": "/static/css/main.0040b4b5.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "83f5455322dfa709d2d2",
+    "url": "/static/js/main.1cc7724d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
